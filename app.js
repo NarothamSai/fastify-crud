c12334ffd38af0eda807a1019fcc1a7b832d5645
NarothamSai <sai.narotham999@gmail.com>
2021-07-23 23:29:59 +0530

    Add fastify swagger

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ const AutoLoad = require('fastify-autoload')
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
 
+  fastify.register(require('fastify-swagger'),{
+    swagger: {
+      info: {
+        title: 'Test swagger',
+        description: 'testing the fastify swagger api',
+        version: '0.1.0'
+      },
+      securityDefinitions: {
+        apiKey: {
+          type: 'apiKey',
+          name: 'apiKey',
+          in: 'header'
+        }
+      },
+      host: 'localhost:3000',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json']
+    },
+    hideUntagged: false,
+    exposeRoute: true
+  })
+
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
