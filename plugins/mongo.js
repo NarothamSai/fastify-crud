7dab3f07d790e0d5bdbc0c28473dff9c2884a750
NarothamSai <sai.narotham999@gmail.com>
2021-07-27 11:29:37 +0530

    Add mongoose

--- plugins/mongo.js
+++ plugins/mongo.js
@@ -1,10 +1,24 @@
 'use strict'
-
+const mongoose = require("mongoose");
 const fp = require('fastify-plugin')
+//
+// module.exports = fp(async function(fastify,opts){
+//   fastify.register(require('fastify-mongodb'),{
+//       forceClose: true,
+//       url: process.env.MONGODB_URL
+//   })
+// })
 
 module.exports = fp(async function(fastify,opts){
-  fastify.register(require('fastify-mongodb'),{
-      forceClose: true,
-      url: process.env.MONGODB_URL
+  mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection.on('connected',()=>{
+    fastify.log.info('Server connected to mongoDB');
+  })
+  mongoose.connection.on('error',err=>{
+    fastify.log.error(err);
   })
 })
