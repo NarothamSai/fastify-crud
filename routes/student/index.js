e9e62c7bf227c9d614059e06d7f7da3a31f08df9
NarothamSai <sai.narotham999@gmail.com>
2021-07-28 11:25:11 +0530

    Add request validations and response schema

--- routes/student/index.js
+++ routes/student/index.js
@@ -3,32 +3,108 @@ const Student = require("../../components/student/index");
 
 const StudentSchema = {
     name : {type : 'string'},
-    age : {type : 'integer'}
+    age : {type : 'integer'},
+    rollno: {type: 'string'}
+}
+
+const StudentResponse = {
+  type : 'object',
+  properties : {
+    _id : {type : 'string'},
+    name : {type : 'string'},
+    age : {type : 'integer'},
+    rollno: {type: 'string'},
+    statusid: {type : 'integer'},
+    createdAt: {type: 'string'},
+    updatedAt: {type: 'string'}
+  }
+}
+
+const ErrorResponse = {
+  "statusCode":  {type : 'integer'},
+  "error": {type: 'string'},
+  "message": {type: 'string'}
 }
 
 const postOpts = {
   schema :{
-    body : StudentSchema
-  },
-  response:{
-    201: {
-      type: 'object',
-      properties:{
-        studentCreatedId: {type : 'string'}
+    "body" : {
+      "type": 'object',
+      "required": ['name', 'age', 'rollno'],
+      "additionalProperties": false,
+      "properties":{
+        "name" : {type : 'string'},
+        "age" : {type : 'integer'},
+        "rollno": {type: 'string'}
+      }
+    },
+    "response":{
+      201: {
+        "type": 'object',
+        properties:{
+          student: StudentResponse
+        }
+      },
+      '4xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      },
+      '5xx': {
+        "type": 'object',
+        properties : ErrorResponse
       }
     }
   }
 }
 
 const getOpts = {
-  response:{
-    200:{
+  schema : {
+    response:{
+      200:{
+        type: 'object',
+        properties:{
+          count : {type : 'integer'},
+          pagination_data:{
+            type : 'array',
+            items : StudentResponse
+          }
+        }
+      },
+      '4xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      },
+      '5xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      }
+    }
+  }
+}
+
+const getOneOpts = {
+  schema : {
+    params:{
       type: 'object',
       properties:{
-        student:{
-          type : 'array',
-          items : StudentSchema
+        _id: {type : 'string', minLength: 12}
+      },
+      required: ['_id']
+    },
+    "response":{
+      200: {
+        "type": 'object',
+        properties:{
+          student: StudentResponse
         }
+      },
+      '4xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      },
+      '5xx': {
+        "type": 'object',
+        properties : ErrorResponse
       }
     }
   }
@@ -43,14 +119,67 @@ const putOpts = {
       },
       required: ['_id']
     },
-    body : StudentSchema
-  },
-  response:{
-    200: {
+    "body" : {
+      "type": 'object',
+      "required": ['name', 'age', 'rollno'],
+      "additionalProperties": false,
+      "properties":{
+        "name" : {type : 'string'},
+        "age" : {type : 'integer'},
+        "rollno": {type: 'string'}
+      }
+    },
+    "response":{
+      200: {
+        "type": 'object',
+        properties:{
+          student: StudentResponse
+        }
+      },
+      '4xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      },
+      '5xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      }
+    }
+  }
+}
+
+const patchOpts = {
+  schema :{
+    params:{
       type: 'object',
       properties:{
-        matchedCount: {type : 'string'},
-        modifiedCount: {type : 'string'}
+        _id: {type : 'string', minLength: 12}
+      },
+      required: ['_id']
+    },
+    "body" : {
+      "type": 'object',
+      "additionalProperties": false,
+      "properties":{
+        "name" : {type : 'string'},
+        "age" : {type : 'integer'},
+        "rollno": {type: 'string'}
+      }
+    },
+    "response":{
+      200: {
+        "type": 'object',
+        properties:{
+          student: StudentResponse
+        }
+      },
+      '4xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      },
+      '5xx': {
+        "type": 'object',
+        properties : ErrorResponse
       }
     }
   }
@@ -64,13 +193,21 @@ const deleteOpts = {
         _id: {type : 'string', minLength: 12}
       },
       required: ['_id']
-    }
-  },
-  response:{
-    200: {
-      type: 'object',
-      properties:{
-        deletedCount: {type : 'string'}
+    },
+    "response":{
+      200: {
+        "type": 'object',
+        properties:{
+          student: StudentResponse
+        }
+      },
+      '4xx': {
+        "type": 'object',
+        properties : ErrorResponse
+      },
+      '5xx': {
+        "type": 'object',
+        properties : ErrorResponse
       }
     }
   }
@@ -80,13 +217,13 @@ module.exports = async function(fastify,opts){
 
   fastify.post("/",postOpts,Student.Controller.create);
 
-  fastify.get("/:_id",getOpts,Student.Controller.getOne);
+  fastify.get("/:_id",getOneOpts,Student.Controller.getOne);
 
   fastify.get("/",getOpts,Student.Controller.getAll);
 
   fastify.put("/:_id", putOpts,Student.Controller.putOne);
 
-  fastify.patch("/:_id", putOpts,Student.Controller.patchOne);
+  fastify.patch("/:_id", patchOpts,Student.Controller.patchOne);
 
   fastify.delete("/:_id",deleteOpts,Student.Controller.deleteOne);
 }
